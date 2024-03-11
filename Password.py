a6c5a60c9490a40e184f4eb3ec8cda433a528b04
SAI-MOHAN-B <bmohan04846@gmail.com>
2024-03-11 10:54:37 +0530

    added the password file

---
+++ Password.py
@@ -0,0 +1,9 @@
+print("Password Program!!!")
+user_data = input("Enter the username program")
+pass_data = input("Enter the password program")
+if pass_data == "mohan123":
+    print("Password authentication is successfull")
+elif pass_data == "":
+    print("You didn't entered anything")
+else:
+    print("Entered password is incorrect!!!!")
