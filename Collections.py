06d325ef5634ed6bc2c8a3512b431fa8bd9393ef
SAI-MOHAN-B <bmohan04846@gmail.com>
2024-03-11 13:23:24 +0530

    added the frequency collection program with finding the duplicates

---
+++ Collections.py
@@ -0,0 +1,7 @@
+from collections import Counter
+a = [12, 13, 13, 12, 14]
+my_counter = Counter(a)
+print(my_counter)
+print(my_counter.keys())
+print(my_counter.items())
+print(my_counter.values())
