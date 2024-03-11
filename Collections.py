a51f626f961546a433544722acd20c6b875d0fb6
SAI-MOHAN-B <bmohan04846@gmail.com>
2024-03-11 13:32:23 +0530

    adding the named tuples

--- Collections.py
+++ Collections.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import Counter,namedtuple
 a = [12, 13, 13, 12, 14]
 my_counter = Counter(a)
 print(my_counter)
@@ -6,3 +6,9 @@ print(my_counter.keys())
 print(my_counter.items())
 print(my_counter.values())
 print("Collections")
+
+# Named Tuple
+Student = namedtuple('Student',['name','age','DOB'])
+print(Student)
+S = Student('mohan','23','1995')
+print(S)
