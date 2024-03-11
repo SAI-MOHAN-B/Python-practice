82d089733673eb90f4043451e4bf4d39d69a3f1a
SAI-MOHAN-B <bmohan04846@gmail.com>
2024-03-11 11:22:11 +0530

    Added the strings based manipulation

--- strings.py
+++ strings.py
@@ -3,3 +3,14 @@ print(our_strings*3)
 
 # Slicing
 print(our_strings[7:9])
+# reversing the strings
+print(our_strings[:-2])
+print(our_strings.upper())
+print(our_strings.lower())
+print(our_strings.endswith("Hello"))
+print(our_strings.startswith("hello"))
+
+# formatting the strings
+name = "guvi"
+age = "geek iitm researchh park"
+print("Hello {} i'm from {}".format(name,age))
