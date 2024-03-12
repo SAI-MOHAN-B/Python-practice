4f34dbaea506b5dfdffc3e08e977f3f1e217ea63
SAI-MOHAN-B <bmohan04846@gmail.com>
2024-03-12 12:34:52 +0530

    Adding the Logging statements

---
+++ Logging.py
@@ -0,0 +1,7 @@
+# Logging
+# The logging Module in Python is a Powerful builkt-in Modules
+import logging
+
+logging.info(f"this is a basics level of Logging")
+logging.warning(f"This is a warning Message")
+logging.critical(f"this is a critical message")
